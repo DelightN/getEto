5fc8edbc38da19a4e3897f063606c93d00cd6aff
DelightN <judodaikimushi@gmail.com>
2017-01-02 22:54:52 +0900

    Delete comment
    delete auto comment

--- Eto.java
+++ Eto.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package *;
-
-/**
- *
- * @author DelightN
- */
 public class Eto {
     public char[] yearAnimals ={'子','丑','寅','卯','辰','巳','午','未','申','酉','戌','亥'};
     public int currentYear;
